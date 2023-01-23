701362f7955a6e7fc742cf19b6f9e0d4f1d99cd8
magnustymoteus <gruzixpotato@gmail.com>
2023-01-23 20:20:30 +0100

    update

--- main.py
+++ main.py
@@ -28,8 +28,26 @@ class TwoThreeFourTree:
         self.root : NodeArray = NodeArray()
     def isEmpty(self):
         return len(self.root.arr) == 0
+    def retrieveItem(self, key):
+        currentNodes = self.root
+        while(currentNodes is not None):
+            if currentNodes.arr[0].key > key:
+                if currentNodes.leftChild is not None:
+                    currentNodes = currentNodes.leftChild
+                else:
+                    break
+            elif currentNodes.arr[len(currentNodes.arr) - 1].key < key:
+                if currentNodes.rightChild is not None:
+                    currentNodes = currentNodes.rightChild
+                else:
+                    break
+            else:
+                for currentNode in currentNodes.arr:
+                    if currentNode.key == key:
+                        return (currentNode.value, True)
+        return (None, False)
     def insertItem(self, node):
-        currentNodes : NodeArray = self.root
+        currentNodes = self.root
         while(not currentNodes.isEmpty()):
             if currentNodes.arr[0].key > node.key:
                 if currentNodes.leftChild is not None:
@@ -44,16 +62,18 @@ class TwoThreeFourTree:
         if len(currentNodes.arr) < 3:
             currentNodes.arr.append(node)
             currentNodes.arr.sort(key=getKeyOfNode)
+        return True
+
 
 t = TwoThreeFourTree()
 print(t.isEmpty())
 print(t.insertItem(createTreeItem(8, 8)))
 print(t.insertItem(createTreeItem(5, 5)))
 print(t.insertItem(createTreeItem(10, 10)))
-#print(t.insertItem(createTreeItem(15, 15)))
-#print(t.isEmpty())
-#print(t.retrieveItem(5)[0])
-#print(t.retrieveItem(5)[1])
+print(t.insertItem(createTreeItem(15, 15)))
+print(t.isEmpty())
+print(t.retrieveItem(5)[0])
+print(t.retrieveItem(5)[1])
 #t.inorderTraverse(print)
 #print(t.save())
 #t.load({'root': [10], 'children': [{'root': [5]}, {'root': [11]}]})
@@ -62,3 +82,23 @@ print(t.insertItem(createTreeItem(10, 10)))
 #print(t.save())
 #print(t.deleteItem(10))
 #print(t.save())
+
+'''
+True
+True
+True
+True
+True
+False
+5
+True
+5
+8
+10
+15
+{'root': [8],'children':[{'root':[5]},{'root':[10,15]}]}
+False
+{'root': [10],'children':[{'root':[5]},{'root':[11,15]}]}
+True
+{'root': [11],'children':[{'root':[5]},{'root':[15]}]}
+'''
