068537e427f67a80917d31e3982a88a0b7799282
magnustymoteus <gruzixpotato@gmail.com>
2023-01-25 14:58:52 +0100

    update

--- main.py
+++ main.py
@@ -45,21 +45,32 @@ def createTreeItem(key,val):
     return Node(key, val)
 def getKeyOfNode(node):
     return node.key
+def getIndexOfKeyInNodes(nodes : NodeArray, key):
+    for x in range(len(nodes.arr)):
+        if nodes.arr[x].key == key:
+            return x
+def findNodeSuccessor(nodes : NodeArray, key):
+    index = getIndexOfKeyInNodes(nodes, key)
+    currentNode = nodes.children[index+1]
+    while not currentNode.hasEmptyChildren():
+        currentNode = currentNode.children[0]
+    return currentNode
+
 
 class TwoThreeFourTree:
     def __init__(self):
         self.root : NodeArray = NodeArray()
     def isEmpty(self):
         return len(self.root.arr) == 0
-    def retrieveItem(self, key):
+    def retrieveItem(self, key, returnNode=False):
         currentNodes = self.root
         while not currentNodes.hasEmptyChildren():
             if key < currentNodes.arr[0].key and currentNodes.children[0] is not None:
                 currentNodes = currentNodes.children[0]
-                break
+                continue
             elif key > currentNodes.arr[len(currentNodes.arr)-1].key and currentNodes.children[len(currentNodes.children)-1] is not None:
                 currentNodes = currentNodes.children[len(currentNodes.children)-1]
-                break
+                continue
             for x in range(len(currentNodes.arr)-1):
                 if key > currentNodes.arr[x].key and key <= currentNodes.arr[x+1].key:
                     currentNodes = currentNodes.children[x+1]
@@ -67,7 +78,7 @@ class TwoThreeFourTree:
             break
         for currentNode in currentNodes.arr:
             if key == currentNode.key:
-                return (currentNode.value, True)
+                return (currentNode.value, True) if not returnNode else (currentNodes, True)
         return (None, False)
     def inorderTraverse(self, func, currentNode=None):
         if currentNode is None:
@@ -141,7 +152,36 @@ class TwoThreeFourTree:
                 currentNode.children.append(childNode)
                 self.load(child, childNode)
     def deleteItem(self, key):
-        pass
+        toBeDeletedResult = self.retrieveItem(key, True)
+        deletedNodes : NodeArray = toBeDeletedResult[0]
+        foundNode = toBeDeletedResult[1]
+        if not foundNode:
+            return False
+        else:
+            if not deletedNodes.hasEmptyChildren():
+                successorNodes = findNodeSuccessor(deletedNodes, key)
+                successorNode = successorNodes.arr[0]
+                successorNodes.arr.pop(0)
+                indexOfDeletedNode = getIndexOfKeyInNodes(deletedNodes, key)
+                deletedNodes.arr[indexOfDeletedNode] = successorNode
+            else:
+                currentNodes = self.root
+                while not currentNodes.hasEmptyChildren():
+                    if key < currentNodes.arr[0].key and currentNodes.children[0] is not None:
+                        currentNodes = currentNodes.children[0]
+                        break
+                    elif key > currentNodes.arr[len(currentNodes.arr) - 1].key and currentNodes.children[
+                        len(currentNodes.children) - 1] is not None:
+                        currentNodes = currentNodes.children[len(currentNodes.children) - 1]
+                        break
+                    for x in range(len(currentNodes.arr) - 1):
+                        if key > currentNodes.arr[x].key and key <= currentNodes.arr[x + 1].key:
+                            currentNodes = currentNodes.children[x + 1]
+                            break
+                    break
+        return True
+
+
 
 
 
@@ -157,12 +197,11 @@ print(t.retrieveItem(5)[0])
 print(t.retrieveItem(5)[1])
 t.inorderTraverse(print)
 print(t.save())
-t.load({'root': [10], 'children': [{'root': [5]}, {'root': [11]}]})
 t.insertItem(createTreeItem(15, 15))
-#print(t.deleteItem(0))
-#print(t.save())
-#print(t.deleteItem(10))
-#print(t.save())
+print(t.deleteItem(0))
+print(t.save())
+print(t.deleteItem(10))
+print(t.save())
 
 '''
 True
